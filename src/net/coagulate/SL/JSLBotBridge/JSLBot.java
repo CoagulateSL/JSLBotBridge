1b44c1445499a8a2ce2336305b43990875187e8d
Iain Price (LUNA) <git@predestined.net>
2020-08-09 15:52:54 +0100

    Add modules for last two repositories, import version numbers, migrate JSLBot standalone to use POM version/GIT build data

---
+++ src/net/coagulate/SL/JSLBotBridge/JSLBot.java
@@ -0,0 +1,41 @@
+package net.coagulate.SL.JSLBotBridge;
+
+import net.coagulate.Core.BuildInfo.JSLBotBuildInfo;
+import net.coagulate.Core.Database.DBConnection;
+import net.coagulate.SL.SLModule;
+
+import javax.annotation.Nonnull;
+import java.util.Date;
+
+public class JSLBot extends SLModule {
+    public final int majorVersion() { return JSLBotBuildInfo.MAJORVERSION; }
+    public final int minorVersion() { return JSLBotBuildInfo.MINORVERSION; }
+    public final int bugFixVersion() { return JSLBotBuildInfo.BUGFIXVERSION; }
+    public final String commitId() { return JSLBotBuildInfo.COMMITID; }
+    public final Date getBuildDate() { return JSLBotBuildInfo.BUILDDATE; }
+
+    net.coagulate.JSLBot.JSLBot bot=null;
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "JSLBot";
+    }
+    @Nonnull
+    public String getDescription() { return "JSLBot imported sources"; }
+
+    public void shutdown() {}
+    public void initialise() {}
+    @Override
+    public void maintenance() {}
+
+    public void startup() {}
+
+    @Override
+    public Object weakInvoke(String command, Object... arguments) { return null; }
+
+    @Override
+    protected int schemaUpgrade(DBConnection db, String schemaname, int currentversion) {
+        return currentversion;
+    }
+}
