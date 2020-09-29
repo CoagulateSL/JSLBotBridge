c4ef1bac2698e214b00f179ca92e99d59a4799dc
Iain Price (LUNA) <git@predestined.net>
2020-09-29 01:34:37 +0100

    Load Avatars module.  Use a recalc thread to UUIDLookup to correct avatar names from SL bot.

--- src/net/coagulate/SL/JSLBotBridge/JSLBotBridge.java
+++ src/net/coagulate/SL/JSLBotBridge/JSLBotBridge.java
@@ -3,10 +3,13 @@ package net.coagulate.SL.JSLBotBridge;
 import net.coagulate.Core.BuildInfo.JSLBotBridgeBuildInfo;
 import net.coagulate.Core.Database.DBConnection;
 import net.coagulate.Core.Tools.ClassTools;
+import net.coagulate.Core.Tools.UnixTime;
+import net.coagulate.JSLBot.Handlers.Avatars;
 import net.coagulate.JSLBot.JSLBot;
 import net.coagulate.JSLBot.Packets.Types.LLUUID;
 import net.coagulate.SL.Config;
 import net.coagulate.SL.Data.EventQueue;
+import net.coagulate.SL.Data.User;
 import net.coagulate.SL.HTML.ServiceTile;
 import net.coagulate.SL.SL;
 import net.coagulate.SL.SLModule;
@@ -15,8 +18,8 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
+import java.util.logging.Logger;
 
 public class JSLBotBridge extends SLModule {
     public final int majorVersion() { return JSLBotBridgeBuildInfo.MAJORVERSION; }
@@ -90,7 +93,7 @@ public class JSLBotBridge extends SLModule {
         botconfig.put("password",Config.getBotPassword());
         botconfig.put("CnC.authoriser","OwnerOnly");
         botconfig.put("handlers",
-                "CnC,Sink,Health,Regions,Teleportation,Agent,Objects,Groups" //todo ,net.coagulate.SL.Bots.BotInterface,net.coagulate.SL.Services.RegionMonitor"+".SimStats"
+                "Avatars,CnC,Sink,Health,Regions,Teleportation,Agent,Objects,Groups" //todo ,net.coagulate.SL.Bots.BotInterface,net.coagulate.SL.Services.RegionMonitor"+".SimStats"
         );
         botconfig.put("loginlocation","home");
         botconfig.put("CnC.authorisation.owneruuid",Config.getBotOwnerUUID());
@@ -121,6 +124,15 @@ public class JSLBotBridge extends SLModule {
             bot.im(new LLUUID(event.getData().getString("uuid")),event.getData().getString("message"));
             event.complete();
         }
+        if (event.getCommandName().equalsIgnoreCase("recalcnames")) {
+            event.claim();
+            if (recalcthread!=null && recalcthread.isAlive()) {
+                event.complete("RecalcThreadAlive");
+            } else {
+                recalcthread=new RecalcThread(event);
+                recalcthread.start();
+            }
+        }
     }
 
     @Override
@@ -139,5 +151,66 @@ public class JSLBotBridge extends SLModule {
     @Override
     protected int schemaUpgrade(DBConnection db, String schemaName, int currentVersion) {
         return currentVersion;
+    }
+
+    private static Thread recalcthread=null;
+
+
+    private class RecalcThread extends Thread {
+        private int casecorrections=0;
+        private int renames=0;
+        public RecalcThread(EventQueue event) {
+            Logger log=SL.log("AvatarNameRecalc");
+            log.info("Starting!");
+            int processed=0;
+            int lastreport= UnixTime.getUnixTime();
+            int starttime=lastreport;
+            Set<User> allusers=User.getAllUsers();
+            List<String> searchfor=new ArrayList<>(10);
+            for (User user:allusers) {
+                searchfor.add(user.getUUID());
+                processed++;
+                if ((processed % 10) ==0 ) {
+                    doLookup(searchfor);
+                    searchfor.clear();
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ignore) {
+                    }
+                    if ((UnixTime.getUnixTime() - lastreport) > 60) {
+                        lastreport = UnixTime.getUnixTime();
+                        double usersPerSecond = (((double) processed) / ((double) (lastreport - starttime)));
+                        double expectRunTime = ((double) (allusers.size() - processed)) / usersPerSecond;
+                        int eta = (int) expectRunTime;
+                        log.info("Processed " + processed + "/" + allusers.size() + ".  ETA: " + UnixTime.duration(eta, true));
+                    }
+                }
+            }
+            if (!searchfor.isEmpty()) { doLookup(searchfor); }
+            log.info("Exiting - processed "+processed+" with "+renames+" renames and "+casecorrections+" case corrections");
+            event.complete();
+        }
+
+        private void doLookup(List<String> searchfor) {
+            Map<String,String> map=((Avatars)(bot.brain().getHandler("Avatars"))).resolveUUIDStrings(searchfor);
+            for (Map.Entry<String,String> entry:map.entrySet()) {
+                String uuid=entry.getKey();
+                String username=entry.getValue();
+                if (username==null) {
+                    SL.log("AvatarNameRecalc").warning("Failed to get an avatar name for " + uuid);
+                } else {
+                    User user = User.findUserKey(uuid);
+                    if (!user.getUsername().equals(username)) {
+                        user.setUsername(username);
+                        if (!user.getUsername().equalsIgnoreCase(username)) {
+                            SL.log("AvatarNameRecalc").info("Avatar rename for " + uuid + " from " + user.getUsername() + " to " + username);
+                            renames++;
+                        } else {
+                            casecorrections++;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
