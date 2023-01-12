f90be55b364c5b71f3223cdf18bd28e13e2fba5c
Iain Price (LUNA) <git@predestined.net>
2023-01-12 18:16:13 +0000

    Reformat

--- src/net/coagulate/SL/JSLBotBridge/JSLBot.java
+++ src/net/coagulate/SL/JSLBotBridge/JSLBot.java
@@ -11,36 +11,52 @@ import java.util.Date;
 import java.util.Map;
 
 public class JSLBot extends SLModule {
-    public final String commitId() { return JSLBotBuildInfo.COMMITID; }
-    public final Date getBuildDate() { return JSLBotBuildInfo.BUILDDATE; }
-
-    net.coagulate.JSLBot.JSLBot bot;
-
-    @Nullable
-    @Override
-    public Map<ServiceTile, Integer> getServices() {
-        return null;
-    }
-
-    @Nonnull
-    @Override
-    public String getName() {
-        return "JSLBot";
-    }
-    @Nonnull
-    public String getDescription() { return "JSLBot imported sources"; }
-
-    public void shutdown() {}
-    public void initialise() {}
-    @Override
-    public void maintenance() {}
-    @Override
-    public void maintenanceInternal() {}
-
-    public void startup() {}
-
-    @Override
-    protected int schemaUpgrade(final DBConnection db, final String schemaName, final int currentVersion) {
-        return currentVersion;
-    }
+	net.coagulate.JSLBot.JSLBot bot;
+	
+	@Nullable
+	@Override
+	public Map<ServiceTile,Integer> getServices() {
+		return null;
+	}
+	
+	@Nonnull
+	@Override
+	public String getName() {
+		return "JSLBot";
+	}
+	
+	@Nonnull
+	public String getDescription() {
+		return "JSLBot imported sources";
+	}
+	
+	public void shutdown() {
+	}
+	
+	public void startup() {
+	}
+	
+	public void initialise() {
+	}
+	
+	@Override
+	public void maintenance() {
+	}
+	
+	@Override
+	public void maintenanceInternal() {
+	}
+	
+	public final String commitId() {
+		return JSLBotBuildInfo.COMMITID;
+	}
+	
+	public final Date getBuildDate() {
+		return JSLBotBuildInfo.BUILDDATE;
+	}
+	
+	@Override
+	protected int schemaUpgrade(final DBConnection db,final String schemaName,final int currentVersion) {
+		return currentVersion;
+	}
 }
